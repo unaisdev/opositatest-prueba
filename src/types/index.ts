0b4f948ae341de667aad9f5018229146fc22e101
unaisdev <unaisdev@gmail.com>
2024-07-31 01:29:21 +0200

    [feat] Add Book types

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export type Books = Book[];
+
+export interface Book {
+  url: string;
+  name: string;
+  isbn: string;
+  authors: string[];
+  numberOfPages: number;
+  publisher: string;
+  country: string;
+  mediaType: string;
+  released: string;
+  characters: string[];
+  povCharacters: string[];
+}
