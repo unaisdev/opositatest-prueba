6649ed4140a8d8372410bbfa097f40703177c8a7
unaisdev <unaisdev@gmail.com>
2024-07-31 01:29:42 +0200

    [feat] Add books fetching service

---
+++ src/services/fetchBooks.ts
@@ -0,0 +1,13 @@
+import {Books} from '../types';
+
+// Obtenemos los libros
+export const fetchBooks = async (): Promise<Books | undefined> => {
+  try {
+    console.log('Fetching books');
+
+    const response = await fetch('https://anapioficeandfire.com/api/books');
+    const data = await response.json();
+
+    return data as Books;
+  } catch (error) {}
+};
