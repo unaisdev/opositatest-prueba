dddda460c900537845484636645ca458eed3c6cd
unaisdev <unaisdev@gmail.com>
2024-07-31 13:10:38 +0200

    [feat] Create Zustand store for state persisting with AsyncStorage

---
+++ src/storage/sorting.ts
@@ -0,0 +1,26 @@
+import {create} from 'zustand';
+import {persist, createJSONStorage} from 'zustand/middleware';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import {SortingEnum} from '../types';
+
+interface SortingTypeState {
+  sortingType: SortingEnum;
+  setSortingType: (sort: SortingEnum) => void;
+}
+
+export const useSortingTypeStore = create<SortingTypeState>()(
+  persist(
+    set => ({
+      sortingType: SortingEnum.DEFAULT,
+      setSortingType: sort => {
+        set({sortingType: sort});
+      },
+    }),
+    {
+      name: 'food-storage', // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => AsyncStorage), // (optional) by default, 'localStorage' is used
+    },
+  ),
+);
