c6ed6015dbcb70cad00793749d55b9b21339f823
unaisdev <unaisdev@gmail.com>
2024-08-08 20:11:33 +0200

    [feat] Install babel-plugin-module-resolver and setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ts', '.tsx'],
+        root: ['.'],
+        alias: {
+          '@api': './src/storage/services',
+          '@navigation': './src/navigation',
+          '@components': './src/components',
+          '@services': './src/services',
+          '@screens': './src/screens',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+          '@storage': './src/storage',
+          '@type': './src/types/index',
+        },
+      },
+    ],
+  ],
 };
