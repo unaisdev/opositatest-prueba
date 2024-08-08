3bb6f4c38b2cefeff09bb6f6c0acea404d00a8a1
unaisdev <unaisdev@gmail.com>
2024-08-08 20:25:23 +0200

    [feat] Consume MMVK storage for persisting Zustand data

---
+++ src/storage/sorting/index.ts
@@ -0,0 +1,17 @@
+import {MMKV} from 'react-native-mmkv';
+import {StateStorage} from 'zustand/middleware';
+
+const storage = new MMKV();
+
+export const mmvkStorage: StateStorage = {
+  setItem: (key, data) => {
+    return storage.set(key, data);
+  },
+  getItem: key => {
+    const value = storage.getString(key);
+    return value ?? null;
+  },
+  removeItem: key => {
+    return storage.delete(key);
+  },
+};
--- src/storage/sorting/sorting.ts
+++ src/storage/sorting/sorting.ts
@@ -1,9 +1,8 @@
 import {create} from 'zustand';
 import {persist, createJSONStorage} from 'zustand/middleware';
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
 import {SortingEnum} from '@type/index';
+import {mmvkStorage} from '.';
 
 interface SortingTypeState {
   sortingType: SortingEnum;
@@ -20,7 +19,7 @@ export const useSortingTypeStore = create<SortingTypeState>()(
     }),
     {
       name: 'food-storage', // name of the item in the storage (must be unique)
-      storage: createJSONStorage(() => AsyncStorage), // (optional) by default, 'localStorage' is used
+      storage: createJSONStorage(() => mmvkStorage), // (optional) by default, 'localStorage' is used
     },
   ),
 );
